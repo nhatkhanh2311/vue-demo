8e84ef97ba97d96d79db831dc70ca6cf7e5828d8
khanhpn <khanhpn@runsystem.net>
2024-01-17 10:32:29 +0700

    Storybook

---
+++ src/stories/FormInputText.stories.ts
@@ -0,0 +1,18 @@
+import { StoryObj } from '@storybook/vue3';
+
+import FormInputText from '../components/FormInputText.vue';
+
+const meta = {
+	title: 'Components/FormInputText',
+	component: FormInputText,
+};
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Normal: Story = {
+	args: {
+		label: 'Input',
+		name: 'input',
+	},
+};

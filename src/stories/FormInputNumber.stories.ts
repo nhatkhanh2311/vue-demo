ff3f5c148be0182c0a37f83dcd9bed721c1e1387
khanhpn <khanhpn@runsystem.net>
2024-01-18 15:05:31 +0700

    Refactor

--- src/stories/FormInputNumber.stories.ts
+++ src/stories/FormInputNumber.stories.ts
@@ -3,16 +3,16 @@ import { StoryObj } from '@storybook/vue3';
 import FormInputNumber from '../components/FormInputNumber.vue';
 
 const meta = {
-	title: 'Components/FormInputNumber',
-	component: FormInputNumber,
+  title: 'Components/FormInputNumber',
+  component: FormInputNumber,
 };
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
 export const Normal: Story = {
-	args: {
-		label: 'Input',
-		name: 'input',
-	},
+  args: {
+    label: 'Input',
+    name: 'input',
+  },
 };
--- src/stories/FormInputText.stories.ts
+++ src/stories/FormInputText.stories.ts
@@ -3,16 +3,16 @@ import { StoryObj } from '@storybook/vue3';
 import FormInputText from '../components/FormInputText.vue';
 
 const meta = {
-	title: 'Components/FormInputText',
-	component: FormInputText,
+  title: 'Components/FormInputText',
+  component: FormInputText,
 };
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
 export const Normal: Story = {
-	args: {
-		label: 'Input',
-		name: 'input',
-	},
+  args: {
+    label: 'Input',
+    name: 'input',
+  },
 };
